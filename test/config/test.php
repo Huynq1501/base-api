47cc3a5cee7d380d204db28c53ed74e67643bea3
quanghuy03092017 <quanghuy03092017@gmail.com>
2022-07-14 14:29:06 +0700

    api config with auth

--- test/config/test.php
+++ test/config/test.php
@@ -43,39 +43,43 @@ $inputData = [
     'language' => 'vietnamese',
     'value' => 31111,
     'label' => 'abc',
-    'type' => 0,
+    'type' => 2,
     'status' => 1,
     'username'=>'hippo_push',
-    'signature'=>'9f277baf49a4bd8f1408e84386db7a65'
+    'signature'=>'8eb1e73941faa30b18352c8e0cc218e7'
 ];
 
 $showData = [
     'id' => 'company-name',
     'language' => 'vietnamese',
+    'username'=>'hippo_push',
+    'signature'=>'ae53d7b281ba1d62ab315301e33b3073'
 ];
 
 $listData = [
     'category' => 'site',
     'page_number' => 2,
     'number_record_of_pages' => 2,
+    'username'=>'hippo_push',
+    'signature'=>'94426fe48898d120d14c300279ee454a'
 ];
-// api list
+//api list
 //$api = new WebServiceConfig($config['OPTIONS']);
 //$api->setSdkConfig($config);
 //$api->setInputData($listData)
 //    ->list();
 
 //api show
-//$api = new WebServiceConfig($config['OPTIONS']);
-//$api->setSdkConfig($config);
-//$api->setInputData($showData)
-//    ->show();
-
-////api  create or update
 $api = new WebServiceConfig($config['OPTIONS']);
 $api->setSdkConfig($config);
-$api->setInputData($inputData)
-    ->createOrUpdate();
+$api->setInputData($showData)
+    ->show();
+
+////api  create or update
+//$api = new WebServiceConfig($config['OPTIONS']);
+//$api->setSdkConfig($config);
+//$api->setInputData($inputData)
+//    ->createOrUpdate();
 
 
 echo "<pre>";
