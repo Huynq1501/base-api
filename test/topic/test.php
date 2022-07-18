07077941ceee52fc1692a0fade9be237a5b88883
quanghuy03092017 <quanghuy03092017@gmail.com>
2022-07-18 11:09:12 +0700

    refactor config

--- test/topic/test.php
+++ test/topic/test.php
@@ -11,32 +11,7 @@
 use nguyenanhung\Backend\BaseAPI\Http\WebServiceTopic;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
-
-$config = [
-    'DATABASE' => [
-        'driver' => 'mysql',
-        'host' => '127.0.0.1',
-        'username' => 'root',
-        'password' => '150115',
-        'database' => 'base_api',
-        'port' => 3306,
-        'prefix' => 'tnv_',
-        'charset' => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-    ],
-    'OPTIONS' => [
-        'showSignature' => true,
-        'debugStatus' => true,
-        'debugLevel' => 'error',
-        'loggerPath' => __DIR__ . '/../tmp/logs/',
-        // Cache
-        'cachePath' => __DIR__ . '/../tmp/cache/',
-        'cacheTtl' => 3600,
-        'cacheDriver' => 'files',
-        'cacheFileDefaultChmod' => 0777,
-        'cacheSecurityKey' => 'BACKEND-SERVICE',
-    ]
-];
+$config = require __DIR__.'/../config.php';
 
 $inputData = [
     'id' => 9,
