715fb492dbdf3fc5f7043fc84150d4136b38f17b
quanghuy03092017 <quanghuy03092017@gmail.com>
2022-07-21 15:39:41 +0700

    add service provider for laravel

---
+++ src/ServiceProviderForLaravel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace nguyenanhung\Backend\BaseAPI;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServiceProviderForLaravel extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '../test/config.php' => config_path('base_api.php'),
+        ]);
+    }
+}
